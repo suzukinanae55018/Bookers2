1d8ba954808824e57f5919d95e4bf37a5be0183a
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-17 14:01:05 +0000

    [Add] booksテーブル作成まで

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "033211d0d9fb4479a3dd09a93f6adc20.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
