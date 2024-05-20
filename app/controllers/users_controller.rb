f2eb7e0d72070adb66bd82b53d9f2abc32be123a
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-20 12:57:50 +0000

    [Add] userupdate追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,9 +28,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
