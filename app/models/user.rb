5bd579a0774bc554fcf80f65bccfe867208545e1
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-21 06:38:21 +0000

    [Update] 文字数制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ has_many :books, dependent: :destroy
 
 has_one_attached :profile_image
 
-validates :name, presence: true, length: { minimum: 2 }
+validates :name, uniqueness: true, length: { in: 2..20 }
+validates :introduction, length: { maximum: 50 }
 
 def get_profile_image(width, height)
   unless profile_image.attached?
