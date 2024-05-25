bdc648ff292d833c8dc4bd1de6d59c0c261b3aeb
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-25 14:52:44 +0000

    [Add] コメント機能途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,7 @@ before_action :is_matching_login_user, only: [:edit, :update,:destroy]
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: :about
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
