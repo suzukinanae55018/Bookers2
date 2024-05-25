a7c08bf1ed3da33139dcf092f738d0825735ae2a
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-25 09:59:44 +0000

    [Add] いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'home/about' => 'homes#about', as: :about
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
