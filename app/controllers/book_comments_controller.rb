89bf44de667fbbaa3ae09eff3d2aa49ae9eb2378
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-05-30 12:24:56 +0000

    [Update] コメント非同期化完成応用課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,21 +2,23 @@ class BookCommentsController < ApplicationController
 
    def create
       book = Book.find(params[:book_id])
-      comment = current_user.book_comments.new(book_comment_params)
-      comment.book_id = book.id
-      comment.save
+      @comment = current_user.book_comments.new(book_comment_params)
+      @comment.book_id = book.id
+      @comment.save
       # redirect_back(fallback_location: root_path)
-      redirect_to request.referer
+      # redirect_to request.referer
    end
 
 
     def destroy
-      # book = Book.find(params[:book_id])
-      # comment = current_user.book_comments.find_by(book_id: book.id)
+      # book = BookComment.find(params[:book_id])
+      # comment = current_user.book_comments.find_by(book_id: book.id)←一致していない
+        　　 # @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])bookcommentから引っ張ってきた:book_idをbook_idに代入する
       # comment.destroy
-      BookComment.find(params[:id]).destroy
+     @comment =  BookComment.find(params[:id])
+     @comment.destroy
       # redirect_back(fallback_location: root_path)
-      redirect_to request.referer
+      # redirect_to request.referer
     end
 
   private
