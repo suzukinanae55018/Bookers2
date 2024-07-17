a4174eb5d99b6f35e2cde3ba59de3b91991cf736
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-17 04:43:22 +0000

    [Add] コメント追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,6 +4,7 @@ class BookCommentsController < ApplicationController
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = book.id
     @comment.save
+    # 外部キーのbook_idにはbookのidを入れる
     # redirect_back(fallback_location: root_path)
     # redirect_to request.referer
   end
