7994c6c819ba17b9ef775e501e14f0b83de3e560
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-18 04:01:04 +0000

    [Add] コメント追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-
+# emailを許可
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
