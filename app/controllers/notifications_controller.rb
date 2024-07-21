06c702f8de11b5d79d2728e2bbf5a98b3a057239
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-21 04:13:17 +0000

    [Add] コメント追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def update
     notification = current_user.notifications.find(params[:id])
     notification.update(read: true)
-    # 通知の種類によるリダイレクトパスの生成
+    # 通知の種類によるリダイレクトパスの生成条件分岐
     case notification.notifiable_type
     when "Book"
       redirect_to book_path(notification.notifiable)
