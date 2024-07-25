c4a9cab0463ba78a87f0dec6944751b32b5ad338
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-25 08:30:18 +0000

    [Add] コメント追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,7 +8,7 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :group_image
-
+# オーナーは？
   def is_owned_by?(user)
     owner.id == user.id
   end
