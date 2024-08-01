da1945321938493a3a866502e007e322e83fbd58
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-08-01 07:14:54 +0000

    [Fix] ゲストログインルーティング修正、turbolinks無効化

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    post "users/guest_sign_in", to: "public/users/sessions#guest_sign_in"
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
